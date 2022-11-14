dff2aa1240847f4866637a9fae448418967f1013
Amin Hassani <gigilibala4@gmail.com>
2022-11-13 18:38:18 -0800

    [Improve] Rename `Settings.tsx` file To `SettingsList`
    To match the component it is exporting.

--- src/screens/AppSettings.tsx
+++ src/screens/AppSettings.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { useContext, useState } from 'react'
 import { SettingsStackParams } from '../components/navigators/SettingsStack'
-import { SettingsList } from '../components/Settings'
+import { SettingsList } from '../components/SettingsList'
 import { DatabaseContext } from '../hooks/DatabaseProvider'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 
@@ -9,6 +9,7 @@ type Props = NativeStackScreenProps<SettingsStackParams, 'App Settings'>
 
 export function AppSettings({ navigation }: Props) {
   const { clearDatabase } = useContext(DatabaseContext)
+  // TODO(gigilibala): Remove the experimental switch.
   const [isSwitchEnabled, setIsSwitchEnabled] = useState<boolean>(false)
 
   return SettingsList([
