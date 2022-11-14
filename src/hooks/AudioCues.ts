90fb52e007582c3e66430361ffd3e327e59999ed
Amin Hassani <gigilibala4@gmail.com>
2022-11-13 22:32:24 -0800

    [Fix] Move `AudioCues` pref setting out of screen
    This should've been in different files anyway.

--- src/hooks/AudioCues.ts
+++ src/hooks/AudioCues.ts
@@ -1,14 +1,53 @@
-import { useEffect } from 'react'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
 import Tts from 'react-native-tts'
 
 interface IAudioCuesApi {
-  speak(text: string): void
+  pref?: AudioCuesPreferences
+  setPref: any // TODO(gigilibala): Find out the correct type
+  speak: (text: string) => void
+}
+
+export type AudioCuesPreferences = {
+  enabled: boolean
+}
+
+export const defaultPref: AudioCuesPreferences = {
+  enabled: true,
 }
 
 export default function useAudioCues(): IAudioCuesApi {
+  const [pref, setPref] = useState<AudioCuesPreferences>()
+  const { setItem, getItem } = useAsyncStorage('@audio_cues_preferences')
+
+  useEffect(() => {
+    if (pref === undefined) {
+      getItem()
+        .then((value) => {
+          if (value === null) {
+            setPref(defaultPref)
+          } else {
+            const parsedPref: AudioCuesPreferences = JSON.parse(value)
+            setPref(parsedPref)
+          }
+        })
+        .catch((error) => {
+          console.error('Unable to read audio cues pref from storage.')
+        })
+    } else {
+      setItem(JSON.stringify(pref))
+        .then(() => {
+          console.log('Setting pref', pref)
+        })
+        .catch((error) => {
+          console.error('Failed to write audio cue pref to storage.')
+        })
+    }
+  }, [pref])
+
   function speak(text: string) {
     Tts.speak(text)
   }
 
-  return { speak }
+  return { speak, pref, setPref }
 }
--- src/screens/AudioCues.tsx
+++ src/screens/AudioCues.tsx
@@ -1,51 +1,15 @@
-import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { useEffect, useState } from 'react'
 import { SettingsStackParams } from '../components/navigators/SettingsStack'
 import { SettingsList } from '../components/SettingsList'
-import useAudioCues from '../hooks/AudioCues'
+import useAudioCues, {
+  AudioCuesPreferences,
+  defaultPref,
+} from '../hooks/AudioCues'
 
 type Props = NativeStackScreenProps<SettingsStackParams, 'Audio Cues'>
 
-type AudioCuesPreferences = {
-  enabled: boolean
-}
-
-const defaultPref: AudioCuesPreferences = {
-  enabled: true,
-}
-
 export function AudioCues({ route, navigation }: Props) {
-  const { speak } = useAudioCues()
-  const [pref, setPref] = useState<AudioCuesPreferences>()
-  const { setItem, getItem } = useAsyncStorage('@audio_cues_preferences')
-
-  useEffect(() => {
-    if (pref === undefined) {
-      getItem()
-        .then((value) => {
-          if (value === null) {
-            setPref(defaultPref)
-          } else {
-            const parsedPref: AudioCuesPreferences = JSON.parse(value)
-            setPref(parsedPref)
-          }
-        })
-        .catch((error) => {
-          console.error('Unable to read audio cues pref from storage.')
-        })
-    } else {
-      setItem(JSON.stringify(pref))
-        .then(() => {
-          console.log('Setting pref', pref)
-        })
-        .catch((error) => {
-          console.error('Failed to write audio cue pref to storage.')
-        })
-    }
-  }, [pref])
-
-  function setPrefsCorrectly(value?: AudioCuesPreferences) {}
+  const { speak, pref, setPref } = useAudioCues()
 
   return SettingsList([
     {
@@ -55,7 +19,7 @@ export function AudioCues({ route, navigation }: Props) {
           title: 'Enabled',
           switchValue: pref?.enabled,
           onPress: () =>
-            setPref((prevValue) => {
+            setPref((prevValue: AudioCuesPreferences) => {
               if (prevValue === undefined) return defaultPref
               const newValue = { ...prevValue } as AudioCuesPreferences
               newValue.enabled = !prevValue.enabled
