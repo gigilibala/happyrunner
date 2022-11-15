ac510b07d317eb6cfb2df27368e949c9a0e9ecdd
Amin Hassani <gigilibala4@gmail.com>
2022-11-14 20:00:21 -0800

    [New] Add Icons for `AppDrawer`

--- src/components/navigators/AppDrawer.tsx
+++ src/components/navigators/AppDrawer.tsx
@@ -1,5 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import IonIcon from 'react-native-vector-icons/Ionicons'
 import { AboutStack, AboutTitle } from './AboutStack'
 import { HomeStack, HomeTitle } from './HomeStack'
 import { SettingsStack, SettingsTitle } from './SettingsStack'
@@ -21,17 +23,47 @@ export function AppDrawer() {
         options={{
           drawerLabel: HomeTitle,
           headerTitle: HomeTitle,
+          drawerIcon: ({ color, size }) => (
+            <Icon.Button
+              name={'home'}
+              color={color}
+              size={size}
+              backgroundColor={'transparent'}
+            />
+          ),
         }}
       />
       <Drawer.Screen
         name="SettingsStack"
         component={SettingsStack}
-        options={{ drawerLabel: SettingsTitle, headerTitle: SettingsTitle }}
+        options={{
+          drawerLabel: SettingsTitle,
+          headerTitle: SettingsTitle,
+          drawerIcon: ({ color, size }) => (
+            <IonIcon.Button
+              name={'settings'}
+              color={color}
+              size={size}
+              backgroundColor={'transparent'}
+            />
+          ),
+        }}
       />
       <Drawer.Screen
         name="AboutStack"
         component={AboutStack}
-        options={{ drawerLabel: AboutTitle, headerTitle: AboutTitle }}
+        options={{
+          drawerLabel: AboutTitle,
+          headerTitle: AboutTitle,
+          drawerIcon: ({ color, size }) => (
+            <Icon.Button
+              name={'info-circle'}
+              color={color}
+              size={size}
+              backgroundColor={'transparent'}
+            />
+          ),
+        }}
       />
     </Drawer.Navigator>
   )
