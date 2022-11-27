1db8da391108502be8c4d2eba3e8d41bde0cfc1a
Amin Hassani <gigilibala4@gmail.com>
2022-11-28 06:00:48 +0800

    [New] Get activity ID in the `ActivityDetails` screen

--- src/screens/home/ActivityDetails.tsx
+++ src/screens/home/ActivityDetails.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Props } from '../RootNavigator'
 
-export default function ActivityDetails() {
+export default function ActivityDetails({
+  route,
+  navigation,
+}: Props<'Activity Details'>) {
+  const { activityId } = route.params
   return (
     <View>
       <Text>ActivityDetails</Text>
